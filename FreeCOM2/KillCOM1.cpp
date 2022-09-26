c23d4472a426ceda7b30fe2948d3c01a926bfd20
flagersgit <flagersmail@gmail.com>
2022-09-25 22:38:08 -0400

    Cleanup unused code

--- FreeCOM2/KillCOM1.cpp
+++ FreeCOM2/KillCOM1.cpp
@@ -20,21 +20,6 @@ IOService* KillCOM1::probe(IOService *provider, SInt32 *score) {
   }
   const OSSymbol *providerName = provider->copyName(gIOServicePlane);
   
-//  OSDictionary *fcom2Matching = IOService::serviceMatching("FreeCOM2");
-//  if (!fcom2Matching) {
-//    SYSLOG(MODULE_SHORT, "Failed to create FreeCOM2 matching dictionary");
-//    return nullptr;
-//  }
-//
-//  DBGLOG(MODULE_SHORT, "Waiting for FreeCOM2");
-//  IOService *fcom2Service = IOService::waitForMatchingService(fcom2Matching);
-//  fcom2Matching->release();
-//
-//  if (!fcom2Service) {
-//    SYSLOG(MODULE_SHORT, "Failed to locate FreeCOM2");
-//    return nullptr;
-//  }
-  
   OSString *kprtDeviceName = OSDynamicCast(OSString, getProperty("KPRT ACPI Device"));
   if (!kprtDeviceName) {
     SYSLOG(MODULE_SHORT, "Failed to get 'KPRT ACPI Device' property, or not an OSString");
@@ -46,50 +31,4 @@ IOService* KillCOM1::probe(IOService *provider, SInt32 *score) {
     return this;
   }
   return nullptr;
-/*
-  bool                  isCom1 = false;
-  OSArray              *provDevMemArray = nullptr;
-  OSCollectionIterator *iterator = nullptr;
-  OSCollectionIterator *iterator2 = nullptr;
-  OSObject             *devMemArr2 = nullptr;
-  IODeviceMemory       *devMemArr2Casted = nullptr;
-  OSDictionary         *devMemDict = nullptr;
-  OSNumber             *addr = nullptr;
-  
-  provider->setProperty("KillCOM1WasHere", kOSBooleanTrue);
-  if (NULL == (provDevMemArray = OSDynamicCast(OSArray, provider->getProperty(gIODeviceMemoryKey)))) {
-    return nullptr;
-  }
-  if (NULL == (iterator = OSCollectionIterator::withCollection(provDevMemArray))) {
-    return nullptr;
-  }
-  while (NULL != (devMemArr2 = iterator->getNextObject())) {
-    
-    if (NULL == (devMemArr2Casted = OSDynamicCast(IODeviceMemory, devMemArr2))) {
-      continue;
-    }
-    if (devMemArr2Casted->getPhysicalAddress() == COM1_PORT_ADDR) {
-      isCom1 = true;
-      break;
-    }
-//    if (NULL == (iterator2 = OSCollectionIterator::withCollection(devMemArr2Casted))) {
-//      continue;
-//    }
-//    devMemDict = OSDynamicCast(OSDictionary, iterator2->getNextObject());
-//    if (NULL == (addr = OSDynamicCast(OSNumber, devMemDict->getObject("address")))) {
-//      continue;
-//    }
-//    if (addr->unsigned16BitValue() == COM1_PORT_ADDR) {
-//      isCom1 = true;
-//      break;
-//    }
-  }
-  
-  OSSafeReleaseNULL(iterator);
-  OSSafeReleaseNULL(iterator2);
-  if (isCom1) {
-    return this;
-  }
-  return nullptr;
-*/
 }
